6d2e518fd21ad7940f6668f921d0b1511d0f0d8a
somto <somto565@gmail.com>
2024-10-03 14:51:21 +0100

    Added Link component to BlogItem, updated BlogList to pass id prop to BlogItem, and fixed button onClick handlers and classnames in BlogList

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -2,7 +2,6 @@ import { blog_data } from '@/assets/assets'
 import React, { useState } from 'react'
 import Blogitem from './BlogItem'
 
-
 const BlogList = () => { 
 
     const [menu, setMenu] = useState('All');
@@ -10,18 +9,18 @@ const BlogList = () => {
   return (
     <div>
         <div className='flex justify-center gap-6 my-10'>
-            <button onClick={()=>setMenu('All')} className={menu==="All"?'bg-prishdw text-white py-1 px-4 rounded-sm':""}>All</button>
-            <button onClick={()=>setMenu('Technology')}className={menu==="Technology"?'bg-prishdw text-white py-1 px-4 rounded-sm':""}>Technology</button>
-            <button onClick={()=>setMenu('Startup')}className={menu==="StartUp"?'bg-prishdw text-white py-1 px-4 rounded-sm':""}>StartUp</button>
-            <button onClick={()=>setMenu('Lifestyle')}className={menu==="LifeStyle"?'bg-prishdw text-white py-1 px-4 rounded-sm':""}>LifeStyle</button>
+            <button onClick={() => setMenu('All')} className={menu === "All" ? 'bg-prishdw text-white py-1 px-4 rounded-sm' : ""}>All</button>
+            <button onClick={() => setMenu('Technology')} className={menu === "Technology" ? 'bg-prishdw text-white py-1 px-4 rounded-sm' : ""}>Technology</button>
+            <button onClick={() => setMenu('Startup')} className={menu === "Startup" ? 'bg-prishdw text-white py-1 px-4 rounded-sm' : ""}>Startup</button>
+            <button onClick={() => setMenu('Lifestyle')} className={menu === "Lifestyle" ? 'bg-prishdw text-white py-1 px-4 rounded-sm' : ""}>Lifestyle</button>
         </div>
         <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
-            {blog_data.filter((item)=> menu==="All"?true:item.category===menu).map((item)=> {
-                return <Blogitem image={item.image} title={item.title} description={item.description} category={item.category} />
+            {blog_data.filter((item, index) => menu === "All" ? true : item.category === menu).map((item, index) => {
+                return <Blogitem key={item.id} id={item.id} image={item.image} title={item.title} description={item.description} category={item.category} />
             })}
         </div> 
     </div>
   )
 }
 
-export default BlogList
+export default BlogList;
