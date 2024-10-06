ade308f92853d4f30dc1ab474754155a507af7c8
somto <somto565@gmail.com>
2024-10-06 17:15:43 +0100

    Added database connection and image upload functionality to blog API route

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,4 +1,6 @@
+import { ConnectDB } from "@/lib/config/db";
 const { NextResponse } = require("next/server");
+import {writeFile} from 'fs/promises'
 
 const LoadDB = async () => {
     await ConnectDB();
@@ -10,4 +12,20 @@ LoadDB();
 export async function GET(request) {
     
     return NextResponse.json({ msg:'API Working' });
+}
+
+export async function POST(request) {
+
+    const formData = await request.forData();
+    const timestamp = Date.now();
+
+    const image = formData.get('image');
+    const imageByteData = await image.arrayBuffer();
+    const buffer = buffer.from(imageByteData)
+    const path = `./public/${timestamp}_${image.name}`
+    await writeFile(path, buffer);
+    const imgURL = `/${timestamp}_${image.name}`
+    console.log(imgURL);
+    returnNextResponse.json({imgURL})
+    
 }
